cfa64845c7b44144d6e7b9ee5e6bbcb9c3361c84
ToddMS <toddsandlerwasd@gmail.com>
2024-03-21 21:42:15 +0000

    Modifications:
    - Added ReactiveFormModule to app.module.ts
    - Added RecipeService to app.module providers to not lose data when swapping between pages
    - Added recipesChange subscription so we know when we create/update/delete recicpes
    - removed all spec files as not needed
    - added onDeleteRecipe to recipe-detail.component.html
    - added red outline css to let us know what part of form are empty/needed
    - created formgroup for adding a new recipe and ingredients
    - Added ts code for all logic when create/updating/deleting recipes/ingredients
    - added ngOnDestroy to recipe-list.component.ts to remove memory leaks
    - added subscrition listener to recipe lsit to update when adding/updating/removing recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import {Recipe} from "./recipe.model";
 import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>
+
   private recipes: Recipe[] = [
     new Recipe(
       "Lasagne",
@@ -36,6 +39,21 @@ export class RecipeService {
 
   getRecipe(index: number): Recipe {
     return this.recipes[index];
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index - 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
