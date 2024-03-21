d476b601b5f1db448fc1d08c0e1a60f9a9f1c116
ToddMS <toddsandlerwasd@gmail.com>
2024-03-21 22:56:12 +0000

    Modifications:
    - Added HttpClientModule to allow us to save to and from firebase server
    - Added resolver to recipe detail and edit component
    - Created data-storage.service.ts service to allow us to post and get to and from firebase server
    - Added click listeners to get and save buttons in header
    - Implemented post and get methods in header .ts files
    - removed dummy data as we now read from server
    - added resolver service to resolve get/edit methods to keep data on web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RecipeService} from "./recipes/recipe.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import {RecipeService} from "./recipes/recipe.service";
         BrowserModule,
         AppRoutingModule,
         FormsModule,
+        HttpClientModule,
         ReactiveFormsModule
     ],
   providers: [ShoppingListService, RecipeService],
